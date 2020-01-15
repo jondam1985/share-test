3a9394b1ab751096c3b047de7cf742963cefa3db
Damian Ruiz <nrt.damian@gmail.com>
2020-01-15 15:51:11 -0500

    Files uploaded

---
+++ index.js
@@ -0,0 +1,15 @@
+const share = document.getElementById("share");
+
+share.addEventListener("click", function() {
+    if (navigator.share) {
+        navigator.share({
+            title: "This is awesome",
+            text: "But wait, there's more!",
+            url: window.location.href
+        })
+        .then(window.alert("than you for sharing"));
+    }
+    else {
+        window.alert("This is mobile-user land boy, and you ain't welcome here")
+    }
+})
